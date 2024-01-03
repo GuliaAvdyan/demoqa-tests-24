76fcaead9537bf18642e34f9c4323a786021e574
guliya <guliya.avdyan@sbermarket.ru>
2024-01-04 02:14:24 +0300

    added junit 5

---
+++ src/test/java/guru/qa/SingleJUnitTest.java
@@ -0,0 +1,44 @@
+package guru.qa;
+
+import org.junit.jupiter.api.*;
+
+public class SingleJUnitTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        //до всех тестов 1 раз
+        System.out.println("###  BeforeAll");
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+        //после всех тестов 1 раз
+        System.out.println("###  AfterAll");
+
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        //перед каждым тестом
+        System.out.println("### BeforeEach");
+    }
+
+    @AfterEach
+    void afterEach() {
+        //перед каждым тестом
+        System.out.println("### AfterEach");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("### first");
+        Assertions.assertTrue(2*2 == 4);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("### second");
+        Assertions.assertTrue(2+3 == 5);
+    }
+}
